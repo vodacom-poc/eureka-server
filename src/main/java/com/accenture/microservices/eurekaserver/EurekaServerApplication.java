4ddf893d2d56f951dbeb65bbea380e6a518395ca
Mahima Agarwal <mahima.agarwal@accenture.com>
2017-07-27 12:17:13 +0530

    updated properties and pom.xml

--- src/main/java/com/accenture/microservices/eurekaserver/EurekaServerApplication.java
+++ src/main/java/com/accenture/microservices/eurekaserver/EurekaServerApplication.java
@@ -3,13 +3,9 @@ package com.accenture.microservices.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 
 @EnableEurekaServer
-@EnableHystrixDashboard
-@EnableTurbine
 @SpringBootApplication
 public class EurekaServerApplication {
 
